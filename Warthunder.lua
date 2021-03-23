b557c4d9ff606d3a861c744e7cf3f7aaf70c34b8
Attack_on_Pig <Attack_on_pig@live.com>
2021-03-23 22:36:31 +0800

    Warthunder Changes

--- Warthunder.lua
+++ Warthunder.lua
@@ -12,12 +12,14 @@ function OnEvent(event, arg)
 end
 MouseFunctionTable.MOUSE_BUTTON_PRESSED[9] = function()
     x, y = GetMousePosition()
+    y = y * 9 / 16
     OutputLogMessage("x = %d, y = %d \n", x, y)
 end
 local miniMapRatio = 1485
 local largeMapRatio = 3457
 MouseFunctionTable.MOUSE_BUTTON_RELEASED[9] = function()
     local nx, ny = GetMousePosition()
+    local ny = ny * 9 / 16
     local dp = math.sqrt(math.pow(nx - x, 2) + math.pow(ny - y, 2))
     local miniLength = dp / miniMapRatio * mapLength
     local largeMap = dp / largeMapRatio * mapLength
